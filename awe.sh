ddd0bc2fd08379640cc8a3878e1591f4c0145c40
Amit <thecancerus@gmail.com>
2020-11-26 09:10:59 +0530

    added aws cli installation. github api token requirement.

--- awe.sh
+++ awe.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #
+#color codes
+CYANBG='\033[0;96m'
+ORANGE='\033[0;36m'
+GREEN='\033[0;92m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
 apt-get install unzip
+apt-get install awscli
 echo 'Setting update date to IST'
 dpkg-reconfigure tzdata
 
@@ -13,7 +20,19 @@ wo stack install
 
 wo stack status
 
-echo 'Setting up Awesome Enterprise';
+
+echo 'Setting up Redis Server';
+add-apt-repository ppa:chris-lea/redis-server
+apt-get update
+apt-get install redis-server php-redis
+
+service redis-server start
+
+printf "${CYANBG}Please enter GITHUB API Access Token for composer ( get from https://github.com/settings/tokens ) :${NC}\n"
+read access_token
+composer config --global github-oauth.github.com $access_token
+
+echo 'Installing Awesome Enterprise Composer Package';
 cd /var/www/ && composer create-project wpoets/awesome-enterprise --no-interaction
 cd awesome-enterprise 
 
@@ -27,13 +46,6 @@ composer require wpoets/payments-handler
 composer require wpoets/pdf-handler
 composer require wpoets/woocommerce-handler
 
-echo 'Setting up Redis Server';
-add-apt-repository ppa:chris-lea/redis-server
-apt-get update
-apt-get install redis-server php-redis
-
-service redis-server start
-
 cd ~
-wget "https://raw.githubusercontent.com/WPoets/aw-setup/master/aw3-setup.sh" && chmod u+x aw3-setup.sh
-echo 'Environment is ready. Use aw3-setup.sh to setup awesome for the site.';
+wget -qO aw3.sh "https://raw.githubusercontent.com/WPoets/aw-setup/master/aw3-setup.sh" && chmod u+x aw3.sh
+echo 'Environment is ready. Use aw3.sh to setup awesome for the site.';
